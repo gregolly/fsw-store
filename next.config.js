c16b2f1a5b8c4f2b094b554fbd9ffdaf425d16db
gregolly <gregollyff@hotmail.com>
2023-10-18 16:11:27 -0400

    chore:  add images remotePatterns to nextconfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'fsw-store.s3.sa-east-1.amazonaws.com'
+        }
+      ]
+    }
+  };
+  
+module.exports = nextConfig;
