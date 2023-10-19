4744091b59746c19eff93f7f4d7bcbc13a183ead
gregolly <gregollyff@hotmail.com>
2023-10-19 12:34:57 -0400

    feat: add keyboards and mouses sectons, and footer

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image"
 import { Categories } from "./components/categories"
 import { prismaClient } from "@/lib/prisma"
 import { ProductList } from "./components/product-list"
+import { CopyrightIcon } from "lucide-react"
+import { SectionTitle } from "./components/section-title"
+import { BannerSection } from "./components/banner-section"
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -12,34 +15,62 @@ export default async function Home() {
     }
   })
 
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: 'keyboards'
+      }
+    }
+  })
+
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: 'mouses'
+      }
+    }
+  })
+
   return (
-    <div className="p-5">
-      <Image 
-        src="/banner-home-01.png" 
-        alt="Ate 55% de desconto esse mes!" 
-        height={0} 
-        width={0}
-        sizes="100vw"
-        className="w-full h-auto"
+    <>
+      <div className="p-5">
+        <BannerSection 
+          src="/banner-home-01.png" 
+          alt="Ate 55% de desconto esse mes!" 
+        />
+
+      <div className="mt-8">
+        <Categories />
+      </div>
+
+      <div className="mt-8">
+        <SectionTitle>Ofertas</SectionTitle>
+        <ProductList products={deals} />
+      </div>
+
+      <BannerSection 
+          src="/banner-home-02.png" 
+          alt="Ate 55% de desconto esse mes em mouses!" 
       />
 
-    <div className="mt-8">
-      <Categories />
-    </div>
+      <div className="mt-8">
+        <SectionTitle>Teclados</SectionTitle>
+        <ProductList products={keyboards} />
+      </div>
 
-    <div className="mt-8">
-      <h1 className="uppercase font-bold mb-2">Ofertas</h1>
-      <ProductList products={deals} />
-    </div>
-
-    <Image 
-        src="/banner-home-02.png" 
-        alt="Ate 55% de desconto esse mes em mouses!" 
-        height={0} 
-        width={0}
-        sizes="100vw"
-        className="w-full h-auto mt-8"
+      <BannerSection 
+          src="/banner-home-03.png" 
+          alt="Ate 20% de desconto em fones!" 
       />
-   </div>
+
+      <div className="mt-8">
+        <SectionTitle>Mouses</SectionTitle>
+        <ProductList products={mouses} />
+      </div>
+    </div>
+      <footer className="flex gap-1 bg-slate-900 items-center px-8 py-5 text-slate-400 mt-14">
+        <CopyrightIcon size={16} /> 2023 Copyright FSW Store
+      </footer>
+    </>
   )
 }
