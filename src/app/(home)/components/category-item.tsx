af2f75a2c7e764ca666f0270c9b833ea89b935ce
gregolly <gregollyff@hotmail.com>
2023-10-20 15:48:09 -0400

    feat: add link to categories

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -1,6 +1,7 @@
 import { Badge } from "@/components/ui/badge"
 import { categoryIcon } from "@/constants/category-icon"
 import { Category } from "@prisma/client"
+import Link from "next/link"
 interface CategoryItemProps {
     category: Category
 }
@@ -8,9 +9,11 @@ interface CategoryItemProps {
 export const CategoryItem = ({ category }: CategoryItemProps) => {
 
     return (
-        <Badge variant="outline" className="py-3 flex justify-center items-center gap-2 rounded-lg">
-            {categoryIcon[category.slug as keyof typeof categoryIcon]}
-            <span className="font-semibold text-xs">{category.name}</span>
-        </Badge>
+        <Link href={`/category/${category.slug}`}>
+            <Badge variant="outline" className="py-3 flex justify-center items-center gap-2 rounded-lg">
+                {categoryIcon[category.slug as keyof typeof categoryIcon]}
+                <span className="font-semibold text-xs">{category.name}</span>
+            </Badge>
+        </Link>
     )
 }
