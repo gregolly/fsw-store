aeffad3e6c6d575a868f0b25fae553c0f07da24d
gregolly <gregollyff@hotmail.com>
2023-10-22 20:22:57 -0400

    feat: change produtlist and section title to components ui

--- src/app/(home)/components/section-title.tsx
+++
@@ -1,7 +0,0 @@
-import { ComponentProps } from 'react'
-
-export const SectionTitle = ({ children, ...props }: ComponentProps<"h1">) => {
-    return (
-        <h1 className="uppercase font-bold mb-2" {...props}>{children}</h1>
-    )
-}
