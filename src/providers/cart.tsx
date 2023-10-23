875bf0c5a68f65451ff3aa89034d91ae69630de7
gregolly <gregollyff@hotmail.com>
2023-10-23 15:52:52 -0400

    feat: remove semicolon

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -38,25 +38,25 @@ export const CartContext = createContext<ICartContext>({
 const CartProvider = ({ children }: { children: ReactNode }) => {
   const [products, setProducts] = useState<CartProduct[]>(
     JSON.parse(localStorage.getItem("@fsw-store/cart-products") || "[]"),
-  );
+  )
 
   useEffect(() => {
     localStorage.setItem("@fsw-store/cart-products", JSON.stringify(products))
-  }, [products]);
+  }, [products])
 
   // Total sem descontos
   const subtotal = useMemo(() => {
     return products.reduce((acc, product) => {
       return acc + Number(product.basePrice) * product.quantity
     }, 0);
-  }, [products]);
+  }, [products])
 
   // Total com descontos
   const total = useMemo(() => {
     return products.reduce((acc, product) => {
       return acc + product.totalPrice * product.quantity
     }, 0);
-  }, [products]);
+  }, [products])
 
   const totalDiscount = subtotal - total
 
@@ -64,7 +64,7 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
     // se o produto já estiver no carrinho, apenas aumente a sua quantidade
     const productIsAlreadyOnCart = products.some(
       (cartProduct) => cartProduct.id === product.id,
-    );
+    )
 
     if (productIsAlreadyOnCart) {
       setProducts((prev) =>
@@ -76,15 +76,15 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
             };
           }
 
-          return cartProduct;
+          return cartProduct
         }),
-      );
+      )
 
-      return;
+      return
     }
 
     // se não, adicione o produto à lista
-    setProducts((prev) => [...prev, product]);
+    setProducts((prev) => [...prev, product])
   };
 
   const decreaseProductQuantity = (productId: string) => {
@@ -101,8 +101,8 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
           return cartProduct;
         })
         .filter((cartProduct) => cartProduct.quantity > 0),
-    );
-  };
+    )
+  }
 
   const increaseProductQuantity = (productId: string) => {
     setProducts((prev) =>
@@ -114,16 +114,16 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
           };
         }
 
-        return cartProduct;
+        return cartProduct
       }),
-    );
-  };
+    )
+  }
 
   const removeProductFromCart = (productId: string) => {
     setProducts((prev) =>
       prev.filter((cartProduct) => cartProduct.id !== productId),
-    );
-  };
+    )
+  }
 
   return (
     <CartContext.Provider
